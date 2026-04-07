e977b9c5ae2053456d448d9f994dd9f977d9a968
ahamedsiammia <md7559263@gmail.com>
2026-04-07 17:50:02 +0600

    firebase login code

---
+++ firebase-login-code.jsx
@@ -0,0 +1,78 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut,updateProfile } from 'firebase/auth';
+import { auth } from '../../firebase/firebase.init';
+import { GoogleAuthProvider } from "firebase/auth";
+
+
+const AuthProvider = ({children}) => {
+    const AuthContext = createContext(null);
+    const provider = new GoogleAuthProvider();
+    const [user , setUser]=useState(null);
+    const [loading , setLoading]=useState(true)
+
+    // register user
+    const registerUser =(email,password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    // login user
+    const signInUser =(email,password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    // google login 
+    const googleLogin =()=>{
+        setLoading(true)
+        return signInWithPopup(auth,provider)
+    }
+
+    // logOut 
+    const logOut =()=>{
+        setLoading(true);
+        return signOut(auth)
+    }
+
+    // update user profile 
+    const updateUserProfile =(profile)=>{
+        return updateProfile(auth.currentUser,profile)
+    }
+    // send a password reset email 
+    const pinResetEmail =(email)=>{
+        setLoading(true);
+        return sendPasswordResetEmail(auth,email);
+    }
+
+    // observed user state 
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth , (currentUser)=>{
+            setUser(currentUser);
+            setLoading(false)
+        })
+
+        return ()=>{
+            unSubscribe();
+        }
+    },[])
+
+    const authInfo ={
+        user,
+        loading,
+        updateUserProfile,
+        registerUser,
+        signInUser,
+        googleLogin,
+        logOut,
+        pinResetEmail,
+    }
+
+    return (
+        <AuthContext  value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
