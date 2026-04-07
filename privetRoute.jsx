dc4bbf9854d5cc5fd800b3b976224f59acd16a2e
ahamedsiammia <md7559263@gmail.com>
2026-04-07 17:46:37 +0600

    added privet route code

---
+++ privetRoute.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import useAuth from '../hooks/useAuth';
+import { Navigate, useLocation } from 'react-router';
+import LoadingPage from '../Pages/Loading/Loading';
+
+const PrivetRoutes = ({children}) => {
+    const {user,loading}=useAuth();
+    const location = useLocation();
+    console.log("location",location);
+
+    // user er opor depant kore route visit korte deua . login nah thakle login page e niya jaua 
+    
+    if(loading){
+        return <LoadingPage></LoadingPage>
+    }
+
+    if(!user){
+        return <Navigate state={location.pathname} to={"/login"}></Navigate>
+    }
+    return children
+};
+
+export default PrivetRoutes;
